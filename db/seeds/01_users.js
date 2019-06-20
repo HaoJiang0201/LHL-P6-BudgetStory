dca1c2bf1cd5d55a7631f7893d22da29a6ea5383
HaoJiang0201 <haojiang0201@gmail.com>
2019-06-20 19:19:13 +0000

    DB files cleanup

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -2,9 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({first_name: 'Dave', last_name: 'Rod', email:'dave@gmail.com', password:'123'}),
-        knex('users').insert({first_name: 'Sam', last_name: 'Door', email:'sam@gmail.com', password:'123'}),
-        knex('users').insert({first_name: 'Bob', last_name: 'Hill', email:'bob@gmail.com', password:'123'})
+        knex('users').insert({first_name: 'Hao', last_name: 'Jiang', email:'haojiang0201@gmail.com', password:'123456'})
       ]);
     });
 }
